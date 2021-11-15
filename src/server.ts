3dda2e8d5979ff113eade669f69da64584167649
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-11-15 10:44:06 -0300

    aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
+
 
 app.get("/", (request, response) => {
   return response.json({ message: "Hello, world" })
+})
+
+app.post("/courses", (request, response) =>{
+  console.log(request.body);
+  const {name} = request.body;
+  return response.json(name)
 })
 
 app.listen(3333, () => console.log("Server is running"))
