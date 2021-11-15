374d08e00c738ae56675c5e3cb694488925abe84
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-11-15 12:44:14 -0300

    aula 5

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,20 @@
 import { request, Router } from "express";
 import {v4 as uuidV4} from 'uuid';
+import { Category } from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request,response) => {
   const {name, description} = request.body;
-  const category = {
-    name, 
-    description,
-    id: uuidV4()
-  }
+  const category = new Category()
   
+  Object.assign(category, {
+    name, description, created_at: new Date()
+  })
+
+
   categories.push(category);
   return response.status(201).send();
 })
